45d66f428285f47b4f7a3e9a3e65210f784e8619
maedakanako0115 <kako.0115maeda@icloud.com>
2023-01-29 01:20:48 +0900

    make users table

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,18 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->autoIncrement();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('remember_token');
+            $table->string('tel');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('image')->nullable();
+            $table->date('birthday');
+            $table->boolean('role')->default(0);
+            $table->boolean('del_flg')->default(0);
+            $table->string('group_id')->nullable();
+            // $table->rememberToken();
             $table->timestamps();
         });
     }
